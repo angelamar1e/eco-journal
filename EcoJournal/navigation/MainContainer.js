52b17ca2daeaf34b85e471c2fd6d1344ed76a904
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-08-01 23:03:08 +0800

    added educate button

--- EcoJournal/navigation/MainContainer.js
+++ EcoJournal/navigation/MainContainer.js
@@ -4,6 +4,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // screens
 import HomeScreen from './screens/home';
+import EducateScreen from './screens/educate';
+
+// screen names
+const homeName = 'Home'
+const educateName = 'Educate';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +17,7 @@ export default function MainContainer () {
         <NavigationContainer>
             <Tab.Navigator>
                 <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name={educateName} component={EducateScreen} />
             </Tab.Navigator>
         </NavigationContainer>
     );
