88f8c1198fb7bb3559079961a55e088a4ed9576b
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-08-01 23:08:46 +0800

    added daily log button

--- EcoJournal/navigation/MainContainer.js
+++ EcoJournal/navigation/MainContainer.js
@@ -5,10 +5,12 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // screens
 import HomeScreen from './screens/home';
 import EducateScreen from './screens/educate';
+import DailyLogScreen from './screens/daily_log';
 
 // screen names
 const homeName = 'Home'
 const educateName = 'Educate';
+const dailyLogName = 'Daily Log';
 
 const Tab = createBottomTabNavigator();
 
@@ -16,8 +18,9 @@ export default function MainContainer () {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name={homeName} component={HomeScreen} />
                 <Tab.Screen name={educateName} component={EducateScreen} />
+                <Tab.Screen name={dailyLogName} component={DailyLogScreen} />  
             </Tab.Navigator>
         </NavigationContainer>
     );
