debc9acf893397d5df84d0902890a0ac7ed1d056
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-08-04 22:05:23 +0800

    created file for list of articles in educate page

---
+++ EcoJournal/navigation/screens/educateList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+const articles = [
+    { id: 1, title: 'Article 1', content: 'Content of Article 1' },
+    { id: 2, title: 'Article 2', content: 'Content of Article 2' },
+    { id: 3, title: 'Article 3', content: 'Content of Article 3' },
+    // sample articles only
+];
+
+const ArticleList = () => {
+    
+}
