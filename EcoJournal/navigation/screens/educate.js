e59415962d30fc6e2c8a3100dc0256a2e9fb4e6c
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-08-04 16:17:47 +0800

    deleted initial code for Educate screen

--- EcoJournal/navigation/screens/educate.js
+++ EcoJournal/navigation/screens/educate.js
@@ -3,12 +3,6 @@ import { View, Text } from 'react-native';
 
 export default function EducateScreen({ navigation }) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Educate" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>
-                Educate
-            </Text>
-        </View>
+    
     );
 }
