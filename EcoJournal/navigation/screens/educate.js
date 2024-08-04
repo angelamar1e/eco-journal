082ae783bfa4465af1b6fb57cede1eb404d8c78d
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-08-04 16:56:38 +0800

    added style to search bar

--- EcoJournal/navigation/screens/educate.js
+++ EcoJournal/navigation/screens/educate.js
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { View, Text, SafeAreaView, TextInput } from 'react-native';
+import { View, Text, SafeAreaView, TextInput, StyleSheet } from 'react-native';
 
 export default function EducateScreen({ navigation }) {
     return (
     <SafeAreaView style={{flex: 1, marginHorizontal: 20}}>
-        <TextInput placeholder='Search'/>
+        <TextInput 
+            placeholder='Search'
+            style={styles.searchBar}
+        />
     </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    searchBar: {
+        paddingHorizontal: 20,
+        paddingVertical: 10, 
+        borderColor: 'grey',
+        borderWidth: 1,
+        borderRadius: 25,
+        marginTop: 20,
+    }
+})
