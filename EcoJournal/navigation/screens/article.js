e4f67f52f4e7eb767d91ccebb0c77c2c8bf4288b
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-08-06 11:36:48 +0800

    state comments and input fields

--- EcoJournal/navigation/screens/article.js
+++ EcoJournal/navigation/screens/article.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 
+const ArticlePage = ({ route }) => {
+    const { article } = route.params;
+    const [comment, setComment] = React.useState('');
+    const [comments, setComments] = React.useState([
+        { id: 1, user: 'User1', comment: 'Great article!' },
+        { id: 2, user: 'User2', comment: 'Very informative.' },
+    ])};
+    
